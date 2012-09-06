0dd96daeda6834c9814494a9c22ae63307550195
Rodrigo Lima Vieira <rodrigodelimavieira@gmail.com>
2012-09-05 23:40:54 -0400

    Apresentando DbTable ao Model
    O model Post é totalmente independente do DbTable. Essa abordagem é
    utilizada pois podemos querer utilizar várias tabelas em um mesmo
    modelo, por isso fica a cargo do desenvolvedor especificar com quais
    tabelas um modelo irá interagir.
    Alguns desenvolvedores ainda utilizam a abordagem de fazer o
    controlador interagir diretamente com classes DbTable. Isto não é
    errado, em projetos pequenos com foco na agilidade, essa abordagem é
    justificável. No entanto vamos trabalhar de forma mais organizada, já
    esperando um crescimento ordenado do sistema.

--- application/models/Post.php
+++ application/models/Post.php
@@ -2,7 +2,15 @@
 
 class Application_Model_Post
 {
-
+    protected  $_table;
+    
+    public function __construct() {
+        $this->_table = new Application_Model_DbTable_Post();
+    }
+    
+    public function getPost($id) {
+        return $this->_table->find($id)->current();
+    }
 
 }
 
