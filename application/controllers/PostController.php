51debe8f28d28f124f18cf84fda24904a4a7ee67
RodrigoJimmy <rodrigodelimavieira@gmail.com>
2012-09-15 14:36:24 -0400

    Revert "Fix $post ao invés de $posts"
    This reverts commit eeb772206a2172ebb30805804a3a161ea53dbccb.

--- application/controllers/PostController.php
+++ application/controllers/PostController.php
@@ -23,8 +23,8 @@ class PostController extends Zend_Controller_Action
 
     public function listAction()
     {
-        $post = new Application_Model_Post();
-        $this->view->posts = $post->getAll(array('order' => 'created DESC'));
+        $posts = new Application_Model_Post();
+        $this->view->posts = $posts->getAll(array('order' => 'created DESC'));
     }
 
     public function createAction()
@@ -48,12 +48,12 @@ class PostController extends Zend_Controller_Action
         $form = new Application_Form_Post;
         $form->setAction('/post/update');
         $form->submit->setLabel('Update');
-        $post = new Application_Model_Post();
+        $posts = new Application_Model_Post();
         
         if($this->_request->isPost()) {
             if($form->isValid($this->_request->getPost())) {
                 $values = $form->getValues();
-                $post->update($values, 'id = ' . $values['id']);
+                $posts->update($values, 'id = ' . $values['id']);
                 
                 $this->_redirect('/post/list');
             } else {
@@ -61,7 +61,7 @@ class PostController extends Zend_Controller_Action
             }
         } else {
             $id = $this->_getParam('id');
-            $post = $post->fetchRow("id = $id")->toArray();
+            $post = $posts->fetchRow("id = $id")->toArray();
             $form->populate($post);
         }
         
