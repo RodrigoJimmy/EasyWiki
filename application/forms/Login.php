445efe9754de05b8994ed47b95d3060afecddc9b
RodrigoJimmy <rodrigodelimavieira@gmail.com>
2012-09-16 15:24:31 -0400

    Autenticação e Autorização
    Formulário de Login
    Mecanismo de autenticação
    Mecanismo de autorização

---
+++ application/forms/Login.php
@@ -0,0 +1,32 @@
+<?php
+
+class Application_Form_Login extends Zend_Form
+{
+
+    public function init()
+    {
+        $this->setname('Login');
+       
+       $username = new Zend_Form_Element_Text('username');
+       $username->setlabel('Login')
+               ->setRequired(true)
+               ->addFilter('StripTags')
+               ->addValidator('NotEmpty');
+       
+       $password = new Zend_Form_Element_Password('password');
+       $password->setLabel('Senha')
+               ->setRequired(true)
+               ->addFilter('StripTags')
+               ->addValidator('NotEmpty');
+       
+       $submit = new Zend_Form_Element_Submit('submit');
+       $submit->setLabel('Entrar');
+       $submit->setAttrib('id', 'Entrar')
+               ->setIgnore(true);
+       
+       $this->addElements(array($username, $password, $submit));
+       $this->setAction('/auth/login')->setMethod('post');
+    }
+
+}
+
--- library/Easywiki/Controller/Action.php
+++ library/Easywiki/Controller/Action.php
@@ -3,7 +3,32 @@
 class Easywiki_Controller_Action extends Zend_Controller_Action
 {
     public function init() {
-        
+        if(Zend_Registry::isRegistered('acl')) {
+            $session = Zend_Registry::get('session');
+            $request = $this->getRequest();
+            
+            $controller = $request->getControllername();
+            $action = $request->getActionName();
+            
+            $resource = $controller;
+            $privilege = $action;
+            
+            $auth = Zend_Auth::getInstance();
+            
+            if($auth->hasIdentity()) {
+                $role = $session->role;
+            } else {
+                $role = 'anonymous';
+            }
+            
+            $acl = Zend_Registry::get('acl');
+            
+            
+            if(!$acl->isAllowed($role, $resource, $privilege)) {
+                $session->errors[] = 'Acesso negado';
+                $this->_redirect('/');
+            }
+        }
     }
 }
 ?>
