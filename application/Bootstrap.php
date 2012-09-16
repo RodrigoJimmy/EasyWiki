bf1eb001aa2c681b1e04c3e676d0dcd5caf73509
RodrigoJimmy <rodrigodelimavieira@gmail.com>
2012-09-16 13:27:06 -0400

    Utils - config e db no bootstrap
    Armazena configurações do application.ini e conexão com o banco de
    dados no Zend_Registry.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -8,6 +8,19 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $session = new Zend_Session_Namespace('Easywiki');
         Zend_Registry::set('session', $session);
     }
-
+    
+    public function _initConfig()
+    {
+        $config = new Zend_Config($this->getApplication()->getOptions(), true);
+        Zend_Registry::set('config', $config);
+    }
+    
+    public function _initDb()
+    {
+        $db = $this->getPluginResource('db')->getDbAdapter();
+        Zend_Db_Table::setDefaultAdapter($db);
+        Zend_Registry::set('db', $db);
+    }
+    
 }
 
